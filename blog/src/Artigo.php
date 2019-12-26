af35dcbd304d0330336eaa5a56649d003bc66fc7
cMarianno <59261601+cMarianno@users.noreply.github.com>
2019-12-26 14:35:14 -0300

    First Blog in PHP

---
+++ blog/src/Artigo.php
@@ -0,0 +1,50 @@
+<?php
+
+class Artigo
+{
+    private $mysql;
+
+    public function __construct(mysqli $mysql)
+    {
+        $this->mysql = $mysql;
+    }
+
+    public function adicionar(string $titulo, string $conteudo): void
+    {
+        $insereArtigo = $this->mysql->prepare('INSERT INTO artigos (titulo, conteudo) VALUES(?,?);');
+        $insereArtigo->bind_param('ss', $titulo, $conteudo);
+        $insereArtigo->execute();
+    }
+
+    public function deletar(string $id): void
+    {
+        $deletaArtigo = $this->mysql->prepare('DELETE FROM artigos WHERE id = ?;');
+        $deletaArtigo->bind_param('s', $id);
+        $deletaArtigo->execute();
+    }
+
+    public function editar(string $titulo, string $conteudo, string $id): void
+    {
+        $editarArtigo = $this->mysql->prepare('UPDATE artigos SET titulo = ?, conteudo = ? WHERE id = ?');
+        $editarArtigo->bind_param('sss', $titulo, $conteudo, $id);
+        $editarArtigo->execute();
+    }
+
+    public function exibirTodos(): array
+    {
+
+        $resultado = $this->mysql->query('SELECT id, titulo, conteudo FROM artigos');
+        $artigos = $resultado->fetch_all(MYSQLI_ASSOC);
+
+        return $artigos;
+    }
+
+    public function encontrarPorId(string $id): array
+    {
+        $selecionaArtigo = $this->mysql->prepare("SELECT id, titulo, conteudo FROM artigos WHERE id = ?");
+        $selecionaArtigo->bind_param('s', $id);
+        $selecionaArtigo->execute();
+        $artigo = $selecionaArtigo->get_result()->fetch_assoc();
+        return $artigo;
+    }
+}

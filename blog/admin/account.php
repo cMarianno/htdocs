e02984ac801b42ae6f0cd8183065d7dc7eeb140c
Caio Mariano <caioms@nc7i.com.br>
2019-12-27 14:58:41 -0300

    First Modification

--- blog/admin/account.php
+++ blog/admin/account.php
@@ -11,26 +11,25 @@ $artigos = $artigo->exibirTodos();
 <html lang="pt-br">
 
 <head>
-    <title>Página administrativa</title>
+    <title>Login</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 
 <body>
     <div id="container">
-        <h1>Página Administrativa</h1>
-        <?php foreach ($artigos as $art) { ?>
+        <h1>Login</h1>
         <div>
             <div id="artigo-admin">
-                <p><?php echo $art['titulo']; ?></p>
                 <nav>
-                    <a class="botao" href="editar-artigo.php?id=<?php echo $art['id']; ?>">Editar</a>
-                    <a class="botao" href="excluir-artigo.php?id=<?php echo $art['id']; ?>">Excluir</a>
+                <label for=""><b>Digite seu usuario</b></label>
+                <input class="campo-form" type="text" name="login" id="login" />
+                <label for=""><b>Digite sua senha</b></label>
+                <input class="campo-form" type="text" name="senha" id="senha" />
                 </nav>
             </div>
         </div>
-        <?php } ?>
-        <a class="botao botao-block" href="adicionar-artigo.php">Adicionar Artigo</a>
+        <a class="botao botao-block" href="adicionar-artigo.php">Login</a>
     </div>
 </body>
 
